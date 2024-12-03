b416a4a0cb641c65e33a0fb1b18a634406a3f64d
Sidqi Averroes <sidqi.averroes26@gmail.com>
2024-12-03 09:12:01 +0700

    1 - 2

--- 1/2. BestTimeToBuyAndSellStock.cs
+++ 1/2. BestTimeToBuyAndSellStock.cs
@@ -1,1 +1,26 @@
 //leetcode: Best Time to Buy and Sell Stock Solution
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int result = 0;
+
+        int lowPrice = prices[0];
+        int highPrice = prices[0];
+        
+        for(int i = 0; i < prices.Length; i++)
+        {
+            if(prices[i] < lowPrice)
+            {
+                lowPrice = prices[i];
+                highPrice = 0;
+            }
+            else if(prices[i] > highPrice)
+            {
+                highPrice = prices[i];
+                int gap = highPrice - lowPrice;
+                if(result < gap) result = gap;
+            }
+        }
+
+        return result;
+    }
+}
