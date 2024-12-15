d64cfeafa2621be0a2b96a5e36359112d0cb25f4
Sidqi Averroes <sidqi.averroes26@gmail.com>
2024-12-15 18:02:55 +0700

    11 - 5

---
+++ 11/5. ValidAnagram.cs
@@ -0,0 +1,38 @@
+//leetcode 242. Valid Anagram
+
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        Dictionary<char, int> map = [];
+
+        for(int i=0; i<s.Length; i++)
+        {
+            if(map.ContainsKey(s[i]))
+            {
+                map[s[i]] += 1;
+            }
+            else
+            {
+                map.Add(s[i], 1);
+            }
+        }
+
+        for(int i=0; i<t.Length; i++)
+        {
+            if(map.ContainsKey(t[i]) && map[t[i]] > 0)
+            {
+                map[t[i]] -= 1;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        foreach(KeyValuePair<char,int> kvp in map)
+        {
+            if(kvp.Value > 0) return false;
+        }
+
+        return true;
+    }
+}
