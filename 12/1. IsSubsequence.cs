c981df818e6b5299b97f2f28af96fe581530eb01
Sidqi Averroes <sidqi.averroes26@gmail.com>
2024-12-16 23:14:44 +0700

    12 - 1

---
+++ 12/1. IsSubsequence.cs
@@ -0,0 +1,23 @@
+//leetcode 392. Is Subsequence
+
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        if(s.Length == 0) return true;
+        
+        int i = 0;
+        int j = 0;
+
+        while(j<t.Length)
+        {
+            if(i >= s.Length) break;
+
+            if(s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.Length;
+    }
+}
