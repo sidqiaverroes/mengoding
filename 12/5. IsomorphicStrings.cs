bb785dff28b010896a24aa391a2bc0d845442872
Sidqi Averroes <sidqi.averroes26@gmail.com>
2024-12-17 16:26:44 +0700

    12 - 5

---
+++ 12/5. IsomorphicStrings.cs
@@ -0,0 +1,25 @@
+//leetcode 205. Isomorphic Strings
+
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+        Dictionary<char, char> map = [];
+        HashSet<char> set = [];
+
+        for(int i=0; i<s.Length; i++)
+        {
+            if(!map.ContainsKey(s[i]))
+            {
+                if(set.Contains(t[i])) return false;
+
+                map.Add(s[i], t[i]);
+                set.Add(t[i]);
+            }
+            else
+            {
+                if(map[s[i]] != t[i]) return false;
+            }
+        }
+
+        return true;
+    }
+}
