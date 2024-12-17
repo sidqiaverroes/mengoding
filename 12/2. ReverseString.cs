92e7f90e7493bf6a1ab59d95bbdd42372d42059d
Sidqi Averroes <sidqi.averroes26@gmail.com>
2024-12-17 13:18:53 +0700

    12 - 2

---
+++ 12/2. ReverseString.cs
@@ -0,0 +1,18 @@
+//leetcode 344. Reverse String
+
+public class Solution {
+    public void ReverseString(char[] s) {
+        int l = 0;
+        int r = s.Length-1;
+
+        while(l<r)
+        {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+
+            l++;
+            r--;
+        }
+    }
+}
