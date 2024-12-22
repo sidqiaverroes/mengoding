c311ec91858799e64efe95b012a590524882a043
Sidqi Averroes <sidqi.averroes26@gmail.com>
2024-12-22 12:44:51 +0700

    16 - 1

---
+++ 16/1. RomanToInteger.cs
@@ -0,0 +1,32 @@
+//leetcode 13. Roman to Integer
+
+public class Solution {
+    public int RomanToInt(string s) {
+        Dictionary<char, int> map = new()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+        };
+
+        int res = map[s[s.Length-1]];
+
+        for(int i=0; i<s.Length-1; i++)
+        {
+            if(map[s[i]] < map[s[i+1]])
+            {
+                res -= map[s[i]];
+            }
+            else
+            {
+                res+= map[s[i]];
+            }
+        }
+
+        return res;
+    }
+}
