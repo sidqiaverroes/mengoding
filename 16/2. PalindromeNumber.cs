e7bb5a00537017ef560d238deba08487533ecec3
Sidqi Averroes <sidqi.averroes26@gmail.com>
2024-12-22 13:30:17 +0700

    16 - 2

---
+++ 16/2. PalindromeNumber.cs
@@ -0,0 +1,27 @@
+//leetcode 9. Palindrome Number
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if(x<0) return false;
+
+        int div = 1;
+        while(x >= 10 * div && div < 100000001)
+        {
+            div *= 10;
+        }
+
+        while(x>0)
+        {
+            int right = x%10;
+            int left = x/div;
+
+            if(left != right) return false;
+
+            x = x%div;
+            x = x/10;
+            div = div/100;
+        }
+
+        return true;
+    }
+}
