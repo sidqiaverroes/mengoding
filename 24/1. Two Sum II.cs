1acac733966f8d4c41f142ab6a35c21c71ca2b21
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-01-03 14:33:29 +0700

    24 - 1

--- 24/1. Two Sum II.cs
+++ 24/1. Two Sum II.cs
@@ -2,6 +2,32 @@
 
 public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
+        //Two Pointer Approach
+        Dictionary<int, int> dict =[];
+
+        int l = 0;
+        int r = numbers.Length-1;
+
+        while(l<r)
+        {
+            if(numbers[l] + numbers[r] == target) break;
+
+            if(numbers[l] + numbers[r] > target)
+            {
+                r--;
+                while(numbers[r+1] == numbers[r]) r--;
+            }
+            else if(numbers[l] + numbers[r] < target)
+            {
+                l++;
+                while(numbers[l-1] == numbers[l]) l++;
+            }
+        }
+
+        return [l+1, r+1];
+    }
+
+    public int[] TwoSum(int[] numbers, int target) {
         Dictionary<int, int> dict =[];
 
         for(int i=0; i<numbers.Length; i++)
