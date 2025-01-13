f48a41680991b15115c21d7d64711255bec313eb
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-01-13 23:27:55 +0700

    27 - 4

--- 27/4. MaxConsecutiveOnes III.cs
+++ 27/4. MaxConsecutiveOnes III.cs
@@ -1,1 +1,38 @@
 //leetcode 1004. Max Consecutive Ones III
+
+public class Solution {
+    public int LongestOnes(int[] nums, int k) {
+        int maxLength = 0;
+        int lIndex = 0, rIndex = 0;
+
+        while(rIndex < nums.Length)
+        {
+            if(k > 0)
+            {
+                if(nums[rIndex] == 0)
+                {
+                    k--;
+                }
+                rIndex++;
+            }
+            else if(k == 0)
+            {  
+                if(nums[rIndex] == 0)
+                {
+                    maxLength = Math.Max(maxLength, rIndex-lIndex);
+                    if(nums[lIndex] != 0) while(nums[lIndex] == 1) lIndex++;
+                    lIndex++;
+                    rIndex++;
+                }
+                else
+                {
+                    rIndex++;
+                }
+            }
+        }
+
+        maxLength = Math.Max(maxLength, rIndex-lIndex);
+
+        return maxLength;
+    }
+}
