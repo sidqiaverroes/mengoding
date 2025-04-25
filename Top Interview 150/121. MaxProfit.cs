b130c99efb5a276d1ab793e54769b492fef4ea8f
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-04-25 20:57:28 +0700

    Top Interview 150 - 121

---
+++ Top Interview 150/121. MaxProfit.cs
@@ -0,0 +1,28 @@
+//121. Best Time to Buy and Sell Stock
+//25 April 2025
+
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int result = 0;
+
+        int lowPrice = prices[0];
+        int highPrice = prices[0];
+        
+        for(int i = 0; i < prices.Length; i++)
+        {
+            if(prices[i] < lowPrice)
+            {
+                lowPrice = prices[i];
+                highPrice = 0;
+            }
+            else if(prices[i] > highPrice)
+            {
+                highPrice = prices[i];
+                int gap = highPrice - lowPrice;
+                if(result < gap) result = gap;
+            }
+        }
+
+        return result;
+    }
+}
