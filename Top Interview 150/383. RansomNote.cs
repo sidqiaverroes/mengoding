32fc76c6145ef5c2ffa752e62623515b0b1123d2
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-05-01 22:44:04 +0700

    Top Interview 150 - 383

---
+++ Top Interview 150/383. RansomNote.cs
@@ -0,0 +1,34 @@
+//383. Ransom Note
+//1 May 2025
+
+public class Solution {
+    public bool CanConstruct(string ransomNote, string magazine) {
+        if(ransomNote.Length > magazine.Length) return false;
+
+        Dictionary<char, int> dict = [];
+        
+        foreach(var m in magazine)
+        {
+            if(dict.ContainsKey(m))
+            {
+                dict[m]++;
+            }
+            else
+            {
+                dict.Add(m, 1);
+            }
+        }
+
+        foreach(var rn in ransomNote)
+        {
+            if(!dict.ContainsKey(rn)) return false;
+            else
+            {
+                if(dict[rn] <= 0) return false;
+                dict[rn]--;
+            }
+        }
+
+        return true;
+    }
+}
