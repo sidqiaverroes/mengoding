c75ddf4270cffafa6a1cf37d6e79000eac582677
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-05-03 21:14:34 +0700

    Top Interview 150 - 189

---
+++ Top Interview 150/189. RotateArray.cs
@@ -0,0 +1,27 @@
+//189. Rotate Array
+//3 May 2025
+
+public class Solution {
+    public void Rotate(int[] nums, int k) {
+        int mod = k % nums.Length;
+
+        if(mod == 0 || nums.Length == 1) return;
+        
+        List<int> numsList = new List<int>(nums);
+
+        int i = 0;
+
+        int index1 = nums.Length - mod;
+
+        while(i < nums.Length)
+        {
+            nums[i] = numsList[index1];
+            
+            if(index1 == nums.Length-1) index1 = -1;
+
+            index1++;
+
+            i++;
+        }
+    }
+}
