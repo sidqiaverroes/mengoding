3bfa11860d5b6946f570f2e385d0e6871ecbe098
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-05-04 08:45:27 +0700

    Top Interview 150 - 274

---
+++ Top Interview 150/274. HIndex.cs
@@ -0,0 +1,26 @@
+//274. H-Index
+//4 May 2025
+
+public class Solution {
+    public int HIndex(int[] citations) {
+        int[] countArray = new int [1001];
+
+        for(int i=0; i<citations.Length; i++)
+        {
+            for(int j=0; j<=citations[i]; j++)
+            {
+                countArray[j]++;
+            }
+        }
+
+        int res = 0;
+
+        for(int i=0; i<countArray.Length; i++)
+        {
+            if(countArray[i]>=i) res = i;
+        }
+
+        return res;
+
+    }
+}
