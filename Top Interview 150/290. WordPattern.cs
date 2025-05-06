632b3e61b4f70d9b87f2a1e9a856f27a23426a0d
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-05-06 10:03:44 +0700

    Top Interview 150 - 290

---
+++ Top Interview 150/290. WordPattern.cs
@@ -0,0 +1,32 @@
+//290. Word Pattern
+//6 May 2025
+
+public class Solution {
+    public bool WordPattern(string pattern, string s) {
+        string[] words = s.Split(" ");
+        Dictionary<char, string> map = [];
+
+        if(words.Length != pattern.Length) return false;
+
+        int index = 0;
+        foreach(var p in pattern)
+        {
+            if(!map.ContainsKey(p))
+            {
+                //Check if the word alredy mapped
+                foreach(var m in map)
+                {
+                    if(m.Value == words[index]) return false;
+                }
+                map.Add(p, words[index]);
+            }
+            else
+            {
+                if(map[p] != words[index]) return false;
+            }
+            index++;
+        }
+
+        return true;
+    }
+}
