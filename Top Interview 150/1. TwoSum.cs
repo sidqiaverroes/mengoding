b8d6778a2a175769faee7d10d3c3ab1bd74ac683
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-05-12 10:19:32 +0700

    Top Interview 150 - 1

---
+++ Top Interview 150/1. TwoSum.cs
@@ -0,0 +1,20 @@
+//1. Two Sum
+//12 May 2025
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if(map.ContainsKey(complement))
+            {
+                return new int[] {map[complement], i};
+            }
+            map[nums[i]] = i;
+        }
+
+        throw new ArgumentException("Not found");
+    }
+}
