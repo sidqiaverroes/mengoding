30b82166b9d030c40b10f3292adfbd1dc774fc0b
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-05-13 10:34:50 +0700

    Top Interview 150 - 202

---
+++ Top Interview 150/202. HappyNumber.cs
@@ -0,0 +1,21 @@
+//202. Happy Number
+//13 May 2025
+
+public class Solution {
+    public bool IsHappy(int n) {
+        if(n < 9)
+        {
+            if(n==1 || n==7) return true;
+            else return false;
+        }
+
+        int res = 0;
+        while(n>0)
+        {
+            res += (n%10) * (n%10);
+            n = n/10;
+        };
+
+        return IsHappy(res);
+    }
+}
