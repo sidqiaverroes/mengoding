73095e0bf7fbc182554cb4379943fefa3644ad40
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-05-14 14:02:34 +0700

    Top Interview 150 - 11

---
+++ Top Interview 150/11. MaxArea.cs
@@ -0,0 +1,24 @@
+//11. Container With Most Water
+//14 May 2025
+
+public class Solution {
+    public int MaxArea(int[] height) {
+        int maxArea = 0;
+
+        int i = 0;
+        int j = height.Length-1;
+
+        while(i<j)
+        {
+            int y = Math.Min(height[i], height[j]);
+            int x = j - i;
+            int area = x*y;
+            maxArea = Math.Max(maxArea, area);
+
+            if(height[i] < height[j]) i++;
+            else j--;
+        }
+
+        return maxArea;
+    }
+}
