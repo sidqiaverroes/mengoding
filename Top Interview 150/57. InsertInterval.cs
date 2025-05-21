c40320ea8f2770164efb5ffddbbf01910cb5eced
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-05-21 23:11:50 +0700

    Top Interview 150 - 57

---
+++ Top Interview 150/57. InsertInterval.cs
@@ -0,0 +1,48 @@
+//57. Insert Interval
+//21 May 2025
+
+public class Solution
+{
+    public int[][] Insert(int[][] intervals, int[] newInterval)
+    {
+        List<int[]> Result = [];
+        int[] temp = newInterval; //store merging interval
+        bool IsInserted = false;
+
+        for (int i = 0; i < intervals.Length; i++)
+        {
+            //overlapping
+            if (temp[0] <= intervals[i][1] && temp[1] >= intervals[i][0])
+            {
+                temp = MergeInterval(temp, intervals[i]);
+            }
+            //not overlapping
+            else
+            {
+                //if newInterval's location found add to result
+                if (temp[1] < intervals[i][0] && !IsInserted)
+                {
+                    Result.Add(temp);
+                    IsInserted = true;
+                }
+
+                Result.Add(intervals[i]);
+            }
+        }
+
+        if (!IsInserted)
+        {
+            Result.Add(temp);
+        }
+
+        return Result.ToArray();
+    }
+
+    private int[] MergeInterval(int[] first, int[] second)
+    {
+        int start = Math.Min(first[0], second[0]);
+        int end = Math.Max(first[1], second[1]);
+
+        return [start, end];
+    }
+}
