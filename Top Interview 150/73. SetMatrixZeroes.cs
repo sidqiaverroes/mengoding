ff41e460b5655c7c2f92533f37436fde676df0eb
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-05-30 19:51:44 +0700

    Top Interview 150 - 73

---
+++ Top Interview 150/73. SetMatrixZeroes.cs
@@ -0,0 +1,41 @@
+//73. Set Matrix Zeroes
+//30 May 2025
+
+public class Solution {
+    public void SetZeroes(int[][] matrix) {
+        int m = matrix.Length; //row
+        int n = matrix[0].Length; //col
+
+        HashSet<int> RowToSet = [];
+        HashSet<int> ColToSet = [];
+
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                int x = matrix[i][j];
+                if(x == 0)
+                {
+                    RowToSet.Add(i);
+                    ColToSet.Add(j);
+                }
+            }
+        }
+
+        foreach(var row in RowToSet)
+        {
+            for(int i=0; i<n; i++)
+            {
+                matrix[row][i] = 0;
+            }
+        }
+
+        foreach(var col in ColToSet)
+        {
+            for(int i=0; i<m; i++)
+            {
+                matrix[i][col] = 0;
+            }
+        }
+    }
+}
