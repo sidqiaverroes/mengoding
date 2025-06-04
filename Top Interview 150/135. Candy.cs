818b8a815752bfc744913e54f2c57d6c4e1e5770
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-06-04 23:14:07 +0700

    Top Interview 150 - 135

---
+++ Top Interview 150/135. Candy.cs
@@ -0,0 +1,24 @@
+//135. Candy
+//4 June 2025
+
+public class Solution {
+    public int Candy(int[] ratings) {
+        int[] arr = new int[ratings.Length];
+        Array.Fill(arr,1);
+
+        for(int i=1; i<ratings.Length; i++)
+        {
+            if(ratings[i] > ratings[i-1]) arr[i] = arr[i-1]+1;
+        }
+
+        for(int i=ratings.Length-2; i >= 0; i--)
+        {
+            if(ratings[i] > ratings[i+1]) arr[i] = Math.Max(arr[i], arr[i+1]+1);
+        }
+
+        int res = 0;
+        foreach(var a in arr) res += a;
+
+        return res;
+    }
+}
