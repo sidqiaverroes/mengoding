7f9aa44926adc6193f35e6948761f9d8a0f2dc5e
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-06-13 21:44:42 +0700

    Top Interview 150 - 35

---
+++ Top Interview 150/35. SearchInsert.cs
@@ -0,0 +1,15 @@
+//35. Search Insert Position
+//13 June 2025
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] >= target) return i;
+        }
+
+        return nums.Length;
+    }
+}
